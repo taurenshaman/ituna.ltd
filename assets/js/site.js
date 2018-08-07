2ca164ef33bec0d7f53ca4bf0a1105494a9f34b7
Jerin Lee <taurenjerin@qq.com>
2018-08-07 17:02:19 +0800

    move to Metro4

---
+++ assets/js/site.js
@@ -0,0 +1,8 @@
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?73209dbd29ae5dbf54f171069981f26b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
